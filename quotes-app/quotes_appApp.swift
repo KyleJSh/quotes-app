e3ceeb971bdd3c72e324c87f91b066ff3d678d5e
Kyle Sherrington <kylejsherrington@gmail.com>
2021-02-03 10:05:50 -0800

    Initial Commit

---
+++ quotes-app/quotes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotes_appApp.swift
+//  quotes-app
+//
+//  Created by Kyle Sherrington on 2021-02-03.
+//
+
+import SwiftUI
+
+@main
+struct quotes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
